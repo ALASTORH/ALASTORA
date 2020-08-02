4ab00ebeda983d2da98d025d99b35cc0831192fa
ALASTORH <68463072+ALASTORH@users.noreply.github.com>
2020-08-02 20:39:30 +0300

    Add files via upload

--- MARCOS.lua
+++ MARCOS.lua
@@ -4881,13 +4881,13 @@ t = "\n🔘| قائمة الزوجات المجموعه \n●ـ▬ـ▬ஜ۩۞۩
 for k,v in pairs(list) do
 local username = database:get(bot_id.."user:Name" .. v)
 if username then
-t = t..""..k.."← الزوجة[@"..username.."]\n"
+t = t..""..k.."← الزوجة [@"..username.."]\n"
 else
 t = t..""..k.."← الزوجة "..v.."\n"
 end
 end
 if #list == 0 then
-t = "⚠️| لا يوجد زوجه"
+t = "⚠️| لا يوجد زوجات"
 end
 send(msg.chat_id_, msg.id_, t)
 end
@@ -4933,6 +4933,75 @@ database:srem(bot_id..'Mrti:User'..msg.chat_id_, result.sender_user_id_)
 tdcli_function ({ID = "GetUser",user_id_ = result.sender_user_id_},function(arg,data) 
 usertext = '\n👤| العضو » ['..data.first_name_..'](t.me/'..(data.username_ or 'ASTORHBOTS')..')'
 status  = '\n📮| الايدي » `'..result.sender_user_id_..'`\n🙋🏻‍♂╿اهــلا عزيزي\n🎫│تم تنزيل الجقمه مرتك بنجاح✔️\nالآن انتم مفترقان☹️💔'
+send(msg.chat_id_, msg.id_, usertext..status)
+end,nil)
+end
+tdcli_function ({ID = "GetMessage",chat_id_ = msg.chat_id_,message_id_ = tonumber(msg.reply_to_message_id_)}, start_function, nil)
+return false
+end
+-----------------------------------------------------
+if text == 'تنزيل قلبي' and Mod(msg) then
+database:del(bot_id..'Qlbi:User'..msg.chat_id_)
+send(msg.chat_id_, msg.id_, '🗑|  تم تنزيل الجميع من داخل قلبك بالمجموعه ')
+end
+if text == ("تاك لقلبي") and Mod(msg) then
+local list = database:smembers(bot_id..'Qlbi:User'..msg.chat_id_)
+t = "\n🔘| قائمة قلبك بالمجموعه \n●ـ▬ـ▬ஜ۩۞۩ஜ▬ـ▬ـ●\n"
+for k,v in pairs(list) do
+local username = database:get(bot_id.."user:Name" .. v)
+if username then
+t = t..""..k.."← قـلـبـي [@"..username.."]\n"
+else
+t = t..""..k.."← قـلبـي "..v.."\n"
+end
+end
+if #list == 0 then
+t = "⚠️| لا يوجد احـد بقلبكـ"
+end
+send(msg.chat_id_, msg.id_, t)
+end
+---------
+if text == ("رفع لقلبي") and tonumber(msg.reply_to_message_id_) ~= 0 and Mod(msg) then
+if AddChannel(msg.sender_user_id_) == false then
+local textchuser = database:get(bot_id..'text:ch:user')
+if textchuser then
+send(msg.chat_id_, msg.id_,'['..textchuser..']')
+else
+send(msg.chat_id_, msg.id_,'🔖| لا تستطيع استخدام البوت يرجى الاشتراك في القناة حتى تتمكن من استخدام الاوامر \n 📌| اشترك هنا ['..database:get(bot_id..'add:ch:username')..']')
+end
+return false
+end
+if database:get(bot_id..'Lock:Add:Bot'..msg.chat_id_) and not Constructor(msg) then
+send(msg.chat_id_, msg.id_,'⚠️|  تم تعطيل الرفع من قبل المنشئين') 
+return false
+end
+function start_function(extra, result, success)
+database:sadd(bot_id..'Qlbi:User'..msg.chat_id_, result.sender_user_id_)
+tdcli_function ({ID = "GetUser",user_id_ = result.sender_user_id_},function(arg,data) 
+usertext = '\n👤| العضو » ['..data.first_name_..'](t.me/'..(data.username_ or 'ASTORHBOTS')..')'
+local  statuss  = '\n📮| الايدي » `'..result.sender_user_id_..'`\n🙋🏻‍♂╿اهــلا عزيزي\n🎫│تم رفع العضو داخل قلبك✔️\n🛠╽تمت ترقيته بنجاح 😻??👋🏻'
+send(msg.chat_id_, msg.id_, usertext..statuss)
+end,nil)
+end
+tdcli_function ({ID = "GetMessage",chat_id_ = msg.chat_id_,message_id_ = tonumber(msg.reply_to_message_id_)}, start_function, nil)
+return false
+end
+
+if (text == ("تنزيل لقلبي")) and msg.reply_to_message_id_ and Mod(msg) then
+if AddChannel(msg.sender_user_id_) == false then
+local textchuser = database:get(bot_id..'text:ch:user')
+if textchuser then
+send(msg.chat_id_, msg.id_,'['..textchuser..']')
+else
+send(msg.chat_id_, msg.id_,'🔖| لا تستطيع استخدام البوت يرجى الاشتراك في القناة حتى تتمكن من استخدام الاوامر \n 📌| اشترك هنا ['..database:get(bot_id..'add:ch:username')..']')
+end
+return false
+end
+function start_function(extra, result, success)
+database:srem(bot_id..'Qlbi:User'..msg.chat_id_, result.sender_user_id_)
+tdcli_function ({ID = "GetUser",user_id_ = result.sender_user_id_},function(arg,data) 
+usertext = '\n👤| العضو » ['..data.first_name_..'](t.me/'..(data.username_ or 'ASTORHBOTS')..')'
+status  = '\n📮| الايدي » `'..result.sender_user_id_..'`\n🙋🏻‍♂╿اهــلا عزيزي\n🎫│تم تنزيله من داخل قلبك✔️\n🛠╽تمت ازالته من قائمة القلوب 😹💔'
 send(msg.chat_id_, msg.id_, usertext..status)
 end,nil)
 end
