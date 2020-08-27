f2b66beb3595535fa7f5c479433dd020cc77b54b
ALASTORH <68463072+ALASTORH@users.noreply.github.com>
2020-08-27 04:24:26 +0300

    Add files via upload

--- MARCOS.lua
+++ MARCOS.lua
@@ -8481,7 +8481,7 @@ send(msg.chat_id_, msg.id_,'⚠| عذرا لا استطيع طرد ادمنية 
 return false  
 end
 if data and data.ID and data.ID == 'Ok' then
-send(msg.chat_id_, msg.id_,'*تم تنفيذ طلبك يلا انقلع|😂*') 
+send(msg.chat_id_, msg.id_,'*تم تنفيذ طلبك يلا انقلع😂✌️*') 
 tdcli_function ({ ID = "ChangeChatMemberStatus", chat_id_ = msg.chat_id_, user_id_ = msg.sender_user_id_, status_ = { ID = "ChatMemberStatusLeft" },},function(arg,ban) end,nil)   
 return false
 end
