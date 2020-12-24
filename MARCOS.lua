3b522e98b7adaa1ec1a288a88e03044d4333a1f6
ALASTORH <68463072+ALASTORH@users.noreply.github.com>
2020-12-24 04:06:02 +0300

    Add files via upload

--- MARCOS.lua
+++ MARCOS.lua
@@ -8727,6 +8727,60 @@ send(msg.chat_id_, msg.id_,texting[Textes])
 end
 end
 -------------------------------
+if text and text:match("^انذار @(.*)$") and Admin(msg) and not database:get(bot_id..'NightRang:inthar:group'..msg.chat_id_) then
+function FunctionStatus(arg, result)
+if (result.id_) then
+if Rank_Checking(result.id_, msg.chat_id_) == true then
+return send(msg.chat_id_, msg.id_, "\nلا تستطيع  حظر , طرد , كتم , تقييد , انذار: "..Get_Rank(result.id_,msg.chat_id_).." ")
+end
+local numinthar = tonumber(database:get(bot_id.."NightRang:inthar"..msg.chat_id_..result.id_) or 0)
+if numinthar == 0 then
+database:set(bot_id.."NightRang:inthar"..msg.chat_id_..result.id_,'1')
+Send_Options(msg,result.id_,"reply","تم اعطائه انذار : 1")  
+elseif numinthar == 1 then
+Send_Options(msg,result.id_,"reply","تم اعطائه انذار : 2")  
+database:set(bot_id.."NightRang:inthar"..msg.chat_id_..result.id_,'2')
+elseif numinthar == 2 then
+Send_Options(msg,result.id_,"reply","تم اعطائه انذار : 2 وتم كتمه")  
+database:del(bot_id.."NightRang:inthar"..msg.chat_id_..result.id_)
+database:sadd(bot_id.."NightRang:Silence:User:Group"..msg.chat_id_, result.id_)
+end
+else
+send(msg.chat_id_, msg.id_,"المعرف غلط ")
+end
+end
+tdcli_function ({ID = "SearchPublicChat",username_ = text:match("^انذار @(.*)$") }, FunctionStatus, nil)
+end
+if text == ("انذار") and tonumber(msg.reply_to_message_id_) ~= 0 and Admin(msg) and not database:get(bot_id..'NightRang:inthar:group'..msg.chat_id_) then
+function FunctionStatus(arg, result)
+if Rank_Checking(result.sender_user_id_, msg.chat_id_) == true then
+return send(msg.chat_id_, msg.id_, "\nلا تستطيع  حظر , طرد , كتم , تقييد , انذار: "..Get_Rank(result.sender_user_id_,msg.chat_id_).." ")
+end
+local numinthar = tonumber(database:get(bot_id.."NightRang:inthar"..msg.chat_id_..result.sender_user_id_) or 0)
+if numinthar == 0 then
+database:set(bot_id.."NightRang:inthar"..msg.chat_id_..result.sender_user_id_,'1')
+Send_Options(msg,result.sender_user_id_,"reply","تم اعطائه انذار : 1")  
+elseif numinthar == 1 then
+Send_Options(msg,result.sender_user_id_,"reply","تم اعطائه انذار : 2")  
+database:set(bot_id.."NightRang:inthar"..msg.chat_id_..result.sender_user_id_,'2')
+elseif numinthar == 2 then
+Send_Options(msg,result.sender_user_id_,"reply","تم اعطائه انذار : 2 وتم كتمه")  
+database:del(bot_id.."NightRang:inthar"..msg.chat_id_..result.sender_user_id_)
+database:sadd(bot_id.."NightRang:Silence:User:Group"..msg.chat_id_, result.sender_user_id_)
+end
+end
+tdcli_function ({ID = "GetMessage",chat_id_ = msg.chat_id_,message_id_ = tonumber(msg.reply_to_message_id_)}, FunctionStatus, nil)
+end
+if text == 'تفعيل الانذار' and Admin(msg) then   
+database:del(bot_id..'NightRang:inthar:group'..msg.chat_id_) 
+Text = '\n تم تفعيل الانذارات' 
+send(msg.chat_id_, msg.id_,Text) 
+end
+if text == 'تعطيل الانذار' and Admin(msg) then  
+database:set(bot_id..'NightRang:inthar:group'..msg.chat_id_,true) 
+Text = '\nتم تعطيل الانذارات' 
+send(msg.chat_id_, msg.id_,Text) 
+end
 if text == ""..(database:get(bot_id..'Name:Bot') or 'الاسطورة').."مغاداره" or text == 'مغاندره' or text == 'مةنغادرة' then  
 if Sudo(msg) and not database:get(bot_id..'Left:Bot'..msg.chat_id_)  then 
 tdcli_function ({ID = "ChangeChatMemberStatus",chat_id_=msg.chat_id_,user_id_=bot_id,status_={ID = "ChatMemberStatusLeft"},},function(e,g) end, nil) 
