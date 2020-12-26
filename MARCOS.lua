1e0847ea6cb90f3569f715dbc609e33591726f87
ALASTORH <68463072+ALASTORH@users.noreply.github.com>
2020-12-26 16:38:58 +0300

    Add files via upload

--- MARCOS.lua
+++ MARCOS.lua
@@ -10900,12 +10900,12 @@ send(msg.chat_id_, msg.id_,Text)
 end 
 if text and text:match('^(.*) بصمه$') and not database:get(bot_id..'dw:bot:api'..msg.chat_id_) then            
 local Ttext = text:match('^(.*) بصمه$') 
-local ytddl = https.request('https://devstorm.ml/sr.php?search='..URL.escape(Ttext))
+local ytddl = https.request('https://harmof.ml/Youtuop/Youtapi.php?search='..URL.escape(Ttext))
 local zxe = JSON.decode(ytddl)
 for k,v in pairs(zxe.results) do
 if k == 1 then
 local msgin = msg.id_/2097152/0.5 
-https.request('https://devstorm.ml/yt.php?url='..v.url..'&token='..token..'&chat='..msg.chat_id_..'&type=mp3&msg='..msgin)
+https.request('https://harmof.ml/Youtuop/Youtapi.php?url='..v.url..'&token='..token..'&chat='..msg.chat_id_..'&type=mp3&msg='..msgin)
 end
 end
 end
